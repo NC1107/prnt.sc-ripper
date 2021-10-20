6b5506798a0faa74080cddb9fddf7f807c07b3eb
Nicholas Conn <188623nc@gmail.com>
2021-10-20 19:43:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 driver = webdriver.Chrome(executable_path="driver/chromedriver_win32 (1)/chromedriver.exe")
 
-
-def GetScreenshot():
-    pass
-
-
 def RipImages(amount):
     count = 0
     for letter1 in alphabet:
